1ebbef9794065bb1944db8288f4cba1262aac28b
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-08-03 19:48:58 +0530

    sizeof

---
+++ datatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    cout << a << endl ;
+
+    char b = 'k';
+    cout << b << endl ;
+
+    bool c = true;
+    cout << c << endl ;
+
+    float d = 1.2;
+    cout << d << endl ;
+
+    double e = 1.23;
+    cout << e << endl ;
+
+    int size = sizeof(e);
+    cout<<"Size: "<<size;
+
+    return 0;
+}
