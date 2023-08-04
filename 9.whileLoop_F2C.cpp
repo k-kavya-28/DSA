61a2d52f008f8132dda7facd84c264c35579529f
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-08-04 15:15:43 +0530

    patterns

---
+++ 9.whileLoop_F2C.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float f,c;
+    cout<<"Enter F: "<<endl;
+    cin>>f;
+    c=(5.0/9.0)*(f-32);
+    cout<<"Celsius: "<<c<<endl;
+    return 0;
+}
