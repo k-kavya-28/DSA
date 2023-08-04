e63bdf54a01ea7ee09e40601fef79537b80a24ea
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-08-04 16:28:06 +0530

    patterns

---
+++ 27.pattern_q17.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n: "<<endl;
+    cin>>n;
+    int i=1;
+    char ch='A';
+    while(i<=n){
+        int j=1;
+        while(j<=i) {
+            cout<<ch<<" ";
+            ch+=1;
+            j+=1;
+        }
+        cout<<endl;
+
+        i+=1;
+    }
+    return 0;
+}
