e93460dd621548f364f25ff6fe90a25a9b9ea2bb
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-08-24 00:30:28 +0530

    priorityQueueSTL

---
+++ 82.priorityQueueSTL.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+    //max heap
+    priority_queue<int> maxi;
+
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> mini;
+
+    maxi.push(1);
+    maxi.push(3);
+    maxi.push(2);
+    maxi.push(0);
+
+    cout<<"maxi size->"<<maxi.size()<<endl;
+    int n=maxi.size();
+    for(int i=0; i<n; i++){
+        cout<<maxi.top()<<endl;
+        maxi.pop();
+    }cout<<endl;
+
+
+    mini.push(5);
+    mini.push(1);
+    mini.push(0);
+    mini.push(4);
+    mini.push(3);
+
+
+    cout<<"mini size->"<<mini.size()<<endl;
+    int m=mini.size();
+    for(int i=0; i<m; i++){
+        cout<<mini.top()<<endl;
+        mini.pop();
+    }cout<<endl;
+
+    cout<<"mini empty or not ->"<<mini.empty()<<endl;
+
+    
+    return 0;
+}
