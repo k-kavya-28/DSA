c7d5937217d47992a302a998e99ee56580ecdf1b
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-23 22:14:47 +0530

    char-intro

---
+++ 108.charIntro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char name[20];
+    cout<<"enter your name: "<<endl;
+    cin>>name;
+    cout<<"Your name is: "<<name<<endl;
+    
+    return 0;
+}
+
+//enter your name: 
+// khushi
+// Your name is: khushi
+
+//enter your name: 
+// khushi kavya
+// Your name is: khushi
