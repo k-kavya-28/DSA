dc399f5e6f5d815d978ee927666835fff3e92730
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-23 22:23:34 +0530

    char-intro

--- 108.charIntro.cpp
+++ 108.charIntro.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int main()
 {
     char name[20];
+    // cout<<"enter your name: "<<endl;
+    // cin>>name;
+    // cout<<"Your name is: "<<name<<endl;
+
     cout<<"enter your name: "<<endl;
     cin>>name;
+    name[2]='\0';
     cout<<"Your name is: "<<name<<endl;
     
     return 0;
