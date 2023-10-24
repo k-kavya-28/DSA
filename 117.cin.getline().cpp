6877b24647ab0a142f56cfbddb539f5dccc45cbd
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-24 14:03:14 +0530

    getline

---
+++ 117.cin.getline().cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    cout<<str;
+    
+    return 0;
+}
+
+//this code is correct my vscode is giving error
