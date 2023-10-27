e8f6bcc19c8d0b2ce31f65293d6acd9d7ec8b091
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-27 12:59:04 +0530

    2D-array-intro

--- 124.2DarrayIntro.cpp
+++ 124.2DarrayIntro.cpp
@@ -21,6 +21,20 @@ int main()
         }
     }
 
+    //taking input row-wise
+    // for(int row=0; row<3; row++){
+    //     for(int col=0; col<4; col++){
+    //         cin>>arr[row][col];
+    //     }
+    // }
+
+    //taking input column-wise
+    // for(int col=0; col<4; col++){
+    //     for(int row=0; row<3; row++){
+    //         cin>>arr[row][col];
+    //     }
+    // }
+
     //print
     for(int i=0; i<3; i++){
         for(int j=0; j<4; j++){
