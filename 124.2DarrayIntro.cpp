da421651cd5f7ae3fc529cfbaf38cf350d769376
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-27 13:43:57 +0530

    2D-array-intro

--- 124.2DarrayIntro.cpp
+++ 124.2DarrayIntro.cpp
@@ -2,24 +2,37 @@
 
 using namespace std;
 
+bool isPresent(int arr[][4], int row, int col, int target){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            if(target==arr[i][j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main()
 {
     //creating 2D array
     int arr[3][4];
+    // int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    // int arr[3][4] = {{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};
 
     //taking input row-wise
-    // for(int i=0; i<3; i++){
-    //     for(int j=0; j<4; j++){
-    //         cin>>arr[i][j];
-    //     }
-    // }
-
-    //taking input column-wise
-    for(int i=0; i<4; i++){
-        for(int j=0; j<3; j++){
-            cin>>arr[j][i];
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            cin>>arr[i][j];
         }
     }
+
+    //taking input column-wise
+    // for(int i=0; i<4; i++){
+    //     for(int j=0; j<3; j++){
+    //         cin>>arr[j][i];
+    //     }
+    // }
 
     //taking input row-wise
     // for(int row=0; row<3; row++){
@@ -41,6 +54,18 @@ int main()
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
+    }
+
+    //searching a target element
+    cout<<"Enter target element to be searched: "<<endl;
+    int target;
+    cin>>target;
+
+    if(isPresent(arr, 3, 4, target)){
+        cout<<"Element present"<<endl;
+    }
+    else{
+        cout<<"Element not found"<<endl;
     }
     
     return 0;
