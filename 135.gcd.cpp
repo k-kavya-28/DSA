2e04e8bb918ef5e279e3c5ef8f227390b460bbf5
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-10-31 22:12:55 +0530

    gcd

---
+++ 135.gcd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int gcd(int a, int b){
+    if(a==0){return b;}
+    if(b==0){return a;}
+    while(a!=b){
+        if(a>b){a-=b;}
+        else{b-=a;}
+    }
+    return a;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter a and b: ";
+    cin>>a>>b;
+    int ans = gcd(a,b);
+    cout<<"gcd is: "<<ans<<endl;
+    
+    return 0;
+}
