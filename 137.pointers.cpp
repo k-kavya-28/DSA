654b05033612d3d6e93781e59216cab16c3712a8
k-kavya-28 <kkavya1_be21@thapar.edu>
2023-11-11 01:55:49 +0530

    pointers

--- 137.pointers.cpp
+++ 137.pointers.cpp
@@ -4,5 +4,11 @@ int main()
 {
     int num = 5;
     cout <<"Address of num is: "<<&num<<endl;
+    int *ptr = &num;
+    cout<<"value: "<<*ptr<<endl;
+    cout<<"address: "<<ptr<<endl;
+
+    cout<<"size of integer is: "<<sizeof(num)<<endl;
+    cout<<"size of pointer is: "<<sizeof(ptr)<<endl;    //same irrespective of the type of pointer
     return 0;
 }
