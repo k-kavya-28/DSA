33c143910de96efcd936f8f8c02396cca317462c
k-kavya-28 <kkavya1_be21@thapar.edu>
2023-11-14 16:41:56 +0530

    recursion

---
+++ 160.arrSumRecursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int sum(int arr[], int size){
+    //base
+    if(size==1){
+        return arr[0];
+    }
+    int ans = arr[0] + sum(arr+1, size-1);
+    return ans;
+}
+
+int main()
+{
+    int arr[5] = {3,2,5,1,6};
+    int ans = sum(arr, 5);
+    cout<<"Sum is: "<<ans<<endl;
+
+
+ return 0;
+}
