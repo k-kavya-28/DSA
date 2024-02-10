14b7f525dd4e2914569fb5aa5cfbac4259b15e4c
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-02-11 00:12:12 +0530

    OOPS

--- 175.class.cpp
+++ 175.class.cpp
@@ -4,16 +4,43 @@
 using namespace std;
 
 class Hero{
+    //properties
+    private:
+    char level;
+
     public:
     int health;
-    char level;
+    
+    //behaviour or function
+    void print(){
+        cout<<level<<endl;
+    }
+
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth(int h){
+        health = h;
+    }
+
+    void setLevel(char lvl){
+        level = lvl;
+    }
 };
 
 int main()
 {
     Hero h1;
+    h1.health = 100;
+    // h1.level = 'D';
     //cout<<sizeof(h1)<<endl;
     cout<<"Health: "<<h1.health<<endl;
-    cout<<"Level: "<<h1.level<<endl;
+    h1.setLevel('F');
+    cout<<"Level: "<<h1.getLevel()<<endl;
     return 0;
 }
