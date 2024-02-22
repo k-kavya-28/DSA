14bfd105d20b8a636c4bcec03fd3bec850d156e9
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-02-22 18:18:14 +0530

    dsa

--- 175.class.cpp
+++ 175.class.cpp
@@ -10,6 +10,12 @@ class Hero{
 
     public:
     int health;
+
+
+    //default constructor aise hota default h but can exclusively make and default wala fir automatically hat jaega
+    Hero(){
+        cout<<"Constructor Called "<<endl;
+    }
     
     //behaviour or function
     void print(){
@@ -35,6 +41,43 @@ class Hero{
 
 int main()
 {
+
+    //showing the use of Hero() constructor, here we have created an object statically
+    cout<<"hi"<<endl;
+    Hero Thor;
+    cout<<"bye"<<endl;
+    
+    //dynamically
+    Hero * h = new Hero;
+    Hero * h = new Hero();  //same as above
+
+
+
+
+
+
+
+    /*
+    //static allocation
+    Hero a;
+    a.setHealth(88);
+    a.setLevel('B');
+    cout<<"health is: "<<a.health<<endl;
+    cout<<"level is: "<<a.getLevel()<<endl;
+
+    //dynamically
+    Hero * b = new Hero;
+    b->setHealth(90);
+    b->setLevel('A');
+    cout<<"health is: "<<(*b).health<<endl;
+    cout<<"level is: "<<(*b).getLevel()<<endl;
+    cout<<"health is: "<<b->health<<endl;
+    cout<<"level is: "<<b->getLevel()<<endl;
+    */
+
+    
+
+
     // Hero h1;
     // h1.health = 100;
     // h1.level = 'D';
