f76cf77659d6acebc8d8effff773112d1456fd48
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-04-07 14:38:00 +0530

    single-inheritance

---
+++ 178.singleInheritance.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class animal {
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak(){
+        cout<<"speaking"<<endl;
+    }
+};
+
+class dog: public animal{
+
+};
+
+int main()
+{
+    dog d;
+    d.speak();
+    return 0;
+}
