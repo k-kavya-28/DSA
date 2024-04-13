e679895f5b6ea068d506f5b746f83c17f4cdc838
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-04-14 01:06:07 +0530

    LL

--- 184.LLimplementation.cpp
+++ 184.LLimplementation.cpp
@@ -13,14 +13,45 @@ class Node{
     }
 };
 
-void insertAtHead() {
+void insertAtHead(Node* &head, int d) {
+    //using reference here because we don't want to create any copy
+    //creating new node
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
     
 }
 
+void print(Node* &head){
+    //using temp because real LL ko nahi chhedna
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+
 int main()
 {   
+    //new node created
     Node* node1 = new Node(10);
-    cout<< node1 -> data <<endl;
-    cout<< node1 -> next <<endl;
+    //cout<< node1 -> data <<endl;
+    //cout<< node1 -> next <<endl;
+
+    //head pointed to node1
+    Node* head = node1;
+    Node* tail = node1; //tail is just like head also, since till now only one node is there so head and tail are same
+    print(head);
+    insertAtHead(head, 12);
+    print(head);
+    insertAtHead(head, 15);
+    print(head);
     return 0;
 }
