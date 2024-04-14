b247cba76682a68b57a6541617a90b2d77c89b31
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-04-14 13:47:17 +0530

    DLL

--- 185.doublyLL.cpp
+++ 185.doublyLL.cpp
@@ -36,12 +36,49 @@ int getLength(Node* head){
     return len;
 }
 
+void insertAtHead(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+
+void insertAtPosition(Node* &head, int position, int d){
+    if(position == 1){
+        insertAtHead(head, d);
+        return;
+    }
+    
+}
+
 int main()
 {
     Node* node1 = new Node(10);
     Node* head = node1;
+    Node* tail = node1;
     print(head);
     cout<<"Length: "<<getLength(head)<<endl;
+
+    insertAtHead(head, 11);
+    print(head);
+    //cout<<"Length: "<<getLength(head)<<endl;
+
+    insertAtHead(head, 13);
+    print(head);
+
+    insertAtHead(head, 8);
+    print(head);
+
+    insertAtTail(tail, 20);
+    print(head);
+    
 
     return 0;
 }
