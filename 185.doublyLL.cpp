ec8000dbd18a0707f1a2d08f9133acbeb98b1058
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-04-14 13:25:40 +0530

    DLL

--- 185.doublyLL.cpp
+++ 185.doublyLL.cpp
@@ -5,7 +5,7 @@ class Node{
     public:
     int data;
     Node* prev;
-    Node* curr;
+    Node* next;
 
     //constructor
     Node(int d){
@@ -15,9 +15,33 @@ class Node{
     }
 };
 
+//imp - traversal of Linked List(doubly)
+void print(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+//getting length of the Linked List
+int getLength(Node* head){
+    Node* temp = head;
+    int len = 0;
+    while(temp != NULL){
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
 int main()
 {
     Node* node1 = new Node(10);
+    Node* head = node1;
+    print(head);
+    cout<<"Length: "<<getLength(head)<<endl;
 
     return 0;
 }
