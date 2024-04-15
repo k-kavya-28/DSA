006d88401211ed82c0219bd2b9575ac3e41164a8
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-04-15 16:26:28 +0530

    middleOfLL

---
+++ 191.middleOfLL_noobProMethod_optimal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        //0 or 1 node
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        //2 nodes
+        if(head->next->next == NULL){
+            return head->next;
+        }
+        //algo for others
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+            }
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
