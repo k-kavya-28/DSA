b0ae191c8232759be115ee4de07f44a7af60ebf4
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-06-06 21:46:02 +0530

    sortStack

---
+++ 201.sortStackUsingRecursion.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+
+void sortedInsert(stack<int> &stack, int num){
+	//base case
+	if(stack.empty() || (!stack.empty() && stack.top()< num)){
+		stack.push(num);
+		return;
+	}
+
+	int n = stack.top();
+	stack.pop();
+
+	sortedInsert(stack, num);
+
+	stack.push(n);
+}
+
+void sortStack(stack<int> &stack)
+{
+	// Write your code here
+
+	//base case
+	if(stack.empty()){
+		return;
+	}
+
+	int num = stack.top();
+	stack.pop();
+
+	//recursive call
+	sortStack(stack);
+
+	sortedInsert(stack, num);
+}
