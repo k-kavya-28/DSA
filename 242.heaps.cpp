5739d00aaa99d2c7b50063b60071fb0549ad3d38
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-07-04 01:45:14 +0530

    deleteFromHeap

--- 242.heaps.cpp
+++ 242.heaps.cpp
@@ -36,6 +36,38 @@ class heap{
                 cout<<arr[i]<<" ";
             }cout<<endl;
         }
+
+
+        void deleteFromHeap(){
+            //delete me kuch ni bhejna as argument
+            if(size == 0){
+                cout<<"nothing to delete"<<endl;
+                return;
+            }
+            //step1: put last element into first index
+            arr[1] = arr[size];
+            //step2: remove last element
+            size--;
+
+            //step3: take root node to its correct position
+            int i = 1;
+            while(i<size){
+                int leftIndex = 2*i;
+                int rightIndex = 2*i+1;
+
+                if(leftIndex < size && arr[i] < arr[leftIndex]){
+                    swap(arr[i], arr[leftIndex]);
+                    i = leftIndex;
+                }
+                else if (rightIndex < size && arr[i] < arr[rightIndex]){
+                    swap(arr[i], arr[rightIndex]);
+                    i = rightIndex;
+                }
+                else{
+                    return;
+                }
+            }
+        }
 };
 
 int main()
@@ -48,6 +80,9 @@ int main()
     h.insert(52);
     h.insert(54);
 
+    h.print();
+
+    h.deleteFromHeap();
     h.print();
 
  return 0;
