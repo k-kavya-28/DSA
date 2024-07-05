fa03ef4a0554fdaa1c0b3b66144757843c120fdd
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-07-06 00:12:50 +0530

    kthLargestSumSubarray

--- 250.kthLargestSumSubarray.cpp
+++ 250.kthLargestSumSubarray.cpp
@@ -1,15 +1,41 @@
-#include<algorithm>
+#include<algorithm> //1st code
+#include<queue> //for 2nd code
 int getKthLargest(vector<int> &arr, int k)
 {
-	vector<int> sumStore;
+	// vector<int> sumStore;
+	// int n = arr.size();
+	// for(int i=0; i<n; i++){
+	// 	int sum = 0;
+	// 	for(int j=i; j<n; j++){
+	// 		sum += arr[j];
+	// 		sumStore.push_back(sum);
+	// 	}
+	// }
+	// sort(sumStore.begin(), sumStore.end());
+	// return sumStore[sumStore.size() - k];
+
+    //TLE maar rha leetcode pe working on naukri
+
+    
+
+
+    //optimised 
 	int n = arr.size();
+	priority_queue<int, vector<int>, greater<int> > pq;
 	for(int i=0; i<n; i++){
 		int sum = 0;
 		for(int j=i; j<n; j++){
 			sum += arr[j];
-			sumStore.push_back(sum);
+			if(pq.size() < k){
+				pq.push(sum);
+			}
+			else{
+				if(sum > pq.top()){
+					pq.pop();
+					pq.push(sum);
+				}
+			}
 		}
 	}
-	sort(sumStore.begin(), sumStore.end());
-	return sumStore[sumStore.size() - k];
+	return pq.top();
 }
