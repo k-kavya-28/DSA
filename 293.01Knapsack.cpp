1c95837d752a394aa98e8f88d71bc8cc2443c089
k-kavya-28 <kkavya1_be21@thapar.edu>
2024-08-07 23:56:12 +0530

    01knapsack

--- 293.01Knapsack.cpp
+++ 293.01Knapsack.cpp
@@ -77,6 +77,65 @@ int solveTab(vector<int>& weight, vector<int>& value, int n, int capacity){
 
 }
 
+int solveSO(vector<int>& weight, vector<int>& value, int n, int capacity){
+	vector<int> prev(capacity+1, 0);
+	vector<int> curr(capacity+1, 0);
+	for(int w = weight[0]; w<= capacity; w++){
+		//base case
+			if(weight[0] <= capacity){
+				prev[w] = value[0];
+			}
+			else{
+				prev[w] = 0;
+			}
+	}
+
+	for(int index = 1; index < n; index++){
+		for(int w = 0; w<= capacity; w++){
+			int include = 0;
+			if(weight[index] <= w){
+				include = value[index] + prev[w - weight[index]];
+			}
+
+			int exclude = 0 + prev[w];
+
+			curr[w] = max(include, exclude);
+		}
+		prev = curr;
+	}
+	return prev[capacity];
+
+}
+
+int solveSO2(vector<int>& weight, vector<int>& value, int n, int capacity){
+	// vector<int> prev(capacity+1, 0);
+	vector<int> curr(capacity+1, 0);
+	for(int w = weight[0]; w<= capacity; w++){
+		//base case
+			if(weight[0] <= capacity){
+				curr[w] = value[0];
+			}
+			else{
+				curr[w] = 0;
+			}
+	}
+
+	for(int index = 1; index < n; index++){
+		for(int w = capacity; w>= 0; w--){
+			int include = 0;
+			if(weight[index] <= w){
+				include = value[index] + curr[w - weight[index]];
+			}
+
+			int exclude = 0 + curr[w];
+
+			curr[w] = max(include, exclude);
+		}
+	}
+	return curr[capacity];
+
+}
+
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
 {
 	//only recursion would give TLE
@@ -88,5 +147,15 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	// vector<vector<int> > dp(n, vector<int> (maxWeight+1, -1));
 	// return solveMem(weight, value, n-1, maxWeight, dp);	//remember n-1 is the last index and not n
 
-	return solveTab(weight, value, n, maxWeight);
+
+	//tabulation
+	// return solveTab(weight, value, n, maxWeight);
+
+
+	//space optimisation - using 2 vectors curr and prev
+	// return solveSO(weight, value, n, maxWeight);
+
+
+	//space optimisation using only 1 vector curr
+	return solveSO2(weight, value, n, maxWeight);
 }
